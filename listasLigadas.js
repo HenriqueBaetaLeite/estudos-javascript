a270f8eb5b24e7012c6948efcc587f55707c1208
HenriqueBaetaLeite <hbaetaleite@gmail.com>
2021-11-30 19:40:05 -0300

    continuando listasLigadas

--- listasLigadas.js
+++ listasLigadas.js
@@ -18,9 +18,27 @@ export default class LinkedList {
     this.equalsFn = equalsFn;
   }
 
-push(element) {
-  const node = new Node(element);
-  
+  push(element) {
+    const node = new Node(element);
+    let current;
+    if (this.head == null) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next != null) {
+        current = current.next;
+      }
+    }
+    this.count += 1;
+  }
 }
 
-}
+const myList = new LinkedList();
+
+myList.push(18);
+
+myList.push(15);
+
+myList.push(23);
+
+console.log(myList);
